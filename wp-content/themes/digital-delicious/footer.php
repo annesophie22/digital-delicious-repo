972db56d7f469e6b4fdfd4da7f57188f8ab10d73
anesofy <anne-sophie@digitalseeds.ch>
2021-03-04 10:35:32 +0000

    mobile footer finished to be coded

--- wp-content/themes/digital-delicious/footer.php
+++ wp-content/themes/digital-delicious/footer.php
@@ -12,9 +12,9 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
-		<div class="dd-footer-container">
+		<div class="dd-footer__container">
 			<?php if ( has_nav_menu( 'footer' ) ) : ?>
-				<nav class="dd-footer-navigation" aria-label="<?php esc_attr_e( 'Footer menu', 'digital-delicious' ); ?>">
+				<nav class="dd-footer__navigation" aria-label="<?php esc_attr_e( 'Footer menu', 'digital-delicious' ); ?>">
 					<?php
 					wp_nav_menu(
 						array(
@@ -24,22 +24,35 @@
 						)
 					);
 					?>
-				</nav><!-- .dd-footer-navigation -->
+				</nav><!-- .dd-footer__navigation -->
 			<?php endif; ?>
-			<div class="dd-footer-attribution">
-				<p>Icons by <a href="https://fontawesome.com/license">Font Awesome</a>. Only colors may have been changed by us.</p>
+			<div class="dd-footer__contact">
+				<ul class="dd-footer__list">
+					<li class="dd-footer__list-item"><?php echo file_get_contents(get_theme_file_uri("/images/icon_phone.svg")) ; ?><a href="tel:+#">076 247 34 19</a></li>
+					<li class="dd-footer__list-item"><?php echo file_get_contents(get_theme_file_uri("/images/icon_fb.svg")) ; ?><a href="https://www.facebook.com/Home-made-food-383350752019254" class="facebook">Home made food (Pho bep Oi)</a></li>
+				</ul>
+				<ul class="dd-footer__list">
+					<li class="dd-footer__list-item">12h-13h // 18h-19h30</li>
+					<li class="dd-footer__list-item">Tuesdays, Thursdays and Saturdays</li>
+				</ul>
 			</div>
-		</div> <!-- end dd-footer-container -->
-		<div class="site-info">
-					<?php
-					/* translators: 1: Theme name, 2: Theme author. */
-					printf( esc_html__( 'Made with love and care by %2$s', 'digital-delicious' ), 'digital-delicious', '<a href="https://www.digitalseeds.net">Anne-Sophie - DigitalSeeds</a>' );
-					?>
-					<!--
-					Modified by SAS on 22.02.21
-					/* translators: 1: Theme name, 2: Theme author.*/
-					/* printf( esc_html__( 'Theme: %1$s by %2$s.', 'digital-delicious' ), 'digital-delicious', '<a href="https://www.digitalseeds.net">Anne-Sophie Sassard</a>' ); -->	
-			</div><!-- .site-info -->
+		</div> <!-- end dd-footer__container -->
+		<div class="dd-footer__attribution u-center-text">
+				<p>Icons by <a href="https://fontawesome.com/license">Font Awesome</a>. Only colors may have been changed by us.</p>
+		</div>
+		<div class="site-info u-center-text">
+			<p>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Made with love and care by %2$s', 'digital-delicious' ), 'digital-delicious','<a href="https://www.digitalseeds.net">Anne-Sophie - DigitalSeeds</a>' );
+				?>
+			</p>
+			<p class="copyright"><?php printf( esc_html__( '© Copyright 2021 - All rights reserved', 'digital-delicious' )); ?></p>
+				<!--
+				Modified by SAS on 22.02.21
+				/* translators: 1: Theme name, 2: Theme author.*/
+				/* printf( esc_html__( 'Theme: %1$s by %2$s.', 'digital-delicious' ), 'digital-delicious', '<a href="https://www.digitalseeds.net">Anne-Sophie Sassard</a>' ); -->	
+		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 

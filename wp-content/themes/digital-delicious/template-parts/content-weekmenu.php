0183aea17c4c403a051f53d6404ad71fd2489557
anesofy <anne-sophie@digitalseeds.ch>
2021-03-09 19:41:26 +0000

    html markup added in the template part

--- wp-content/themes/digital-delicious/template-parts/content-weekmenu.php
+++ wp-content/themes/digital-delicious/template-parts/content-weekmenu.php
@@ -14,30 +14,19 @@
 
    <!-- CARD DISH -->
    <div class="card-dish">
-      
-      <div class="card-dish__img-container">
-         <img class="card-dish__img" src="<?php echo get_field("dish_img"); ?>" alt="">
-      </div>
       <?php 
-
       $relatedDish = get_field("dishes_today");
-
-      echo "<h3 class='heading-secondary card-dish__name'>";
          foreach($relatedDish as $dish) {
-            echo get_the_title($dish);
-            echo "<br>";
-            echo get_field( "dish_price", $dish->ID );
-            echo "<br>";
-            echo get_field( "dish_desc", $dish->ID );
-            echo "<br>";
-            echo get_the_post_thumbnail($dish->ID);
-         }
-      echo "</h3>";
       ?>
-
-      
-
-      <p class="card-dish__price">Prix</p>
-      <p class="card-dish__desc">Desc</p>
+         <div class="card-dish__img-container">
+            <img class="card-dish__img" src="<?php echo get_the_post_thumbnail($dish->ID); ?>">
+         </div>
+         <h3 class="heading-secondary card-dish__name"><?php echo get_the_title($dish); ?></h3>
+         <p class="card-dish__price"><?php echo get_field( "dish_price", $dish->ID ); ?></p>
+         <p class="card-dish__desc"><?php echo get_field( "dish_desc", $dish->ID ); ?></p>
    </div> <!-- end card-dish --> 
+      <?php
+         } // end foreach()
+      ?>
+   
 </div> 

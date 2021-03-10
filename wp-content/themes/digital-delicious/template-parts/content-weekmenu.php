dda26b3381056d029bf124de79e6bd1edb1c8701
anesofy <anne-sophie@digitalseeds.ch>
2021-03-10 18:20:46 +0000

    added a div to separate each day

--- wp-content/themes/digital-delicious/template-parts/content-weekmenu.php
+++ wp-content/themes/digital-delicious/template-parts/content-weekmenu.php
@@ -9,25 +9,28 @@
 ?>
 
 <div>
-   <!-- card dish -->
+
    <p class="week-menu__day"><mark class="highlighted"><?php the_title(); ?></mark></p>
 
    <?php
    $relatedDish = get_field("dishes_today");
    foreach ($relatedDish as $dish) {
    ?>
-      <div class="card-dish__img-container">
-         <?php echo get_the_post_thumbnail($dish->ID, "", array(
-            "class" => "card-dish__img",
-            "alt" => ""
-         )); ?>
-      </div>
-      <h3 class="heading-secondary card-dish__name"><?php echo get_the_title($dish); ?></h3>
-      <p class="card-dish__price"><?php echo get_field("dish_price", $dish->ID); ?></p>
-      <p class="card-dish__desc"><?php echo get_field("dish_desc", $dish->ID); ?></p>
+      <div>
+         <!-- card dish -->
+         <div class="card-dish__img-container">
+            <?php echo get_the_post_thumbnail($dish->ID, "", array(
+               "class" => "card-dish__img",
+               "alt" => ""
+            )); ?>
+         </div>
+         <h3 class="heading-secondary card-dish__name"><?php echo get_the_title($dish); ?></h3>
+         <p class="card-dish__price"><?php echo get_field("dish_price", $dish->ID); ?></p>
+         <p class="card-dish__desc"><?php echo get_field("dish_desc", $dish->ID); ?></p>
+      </div> <!-- end card dish -->
    <?php
    }
    ?>
-</div> <!-- end card dish -->
+</div>
 
 <hr class="week-menu__hr">

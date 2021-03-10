02db14457d5f04956252cabe46ca5f79c8125964
anesofy <anne-sophie@digitalseeds.ch>
2021-03-10 18:29:09 +0000

    added an "else" in case no dish is cooked on that day

--- wp-content/themes/digital-delicious/template-parts/content-weekmenu.php
+++ wp-content/themes/digital-delicious/template-parts/content-weekmenu.php
@@ -14,22 +14,27 @@
 
    <?php
    $relatedDish = get_field("dishes_today");
-   foreach ($relatedDish as $dish) {
+   if ($relatedDish) {
+      foreach ($relatedDish as $dish) {
    ?>
-      <div>
-         <!-- card dish -->
-         <div class="card-dish__img-container">
-            <?php echo get_the_post_thumbnail($dish->ID, "", array(
-               "class" => "card-dish__img",
-               "alt" => ""
-            )); ?>
-         </div>
-         <h3 class="heading-secondary card-dish__name"><?php echo get_the_title($dish); ?></h3>
-         <p class="card-dish__price"><?php echo get_field("dish_price", $dish->ID); ?></p>
-         <p class="card-dish__desc"><?php echo get_field("dish_desc", $dish->ID); ?></p>
-      </div> <!-- end card dish -->
+         <div>
+            <!-- card dish -->
+            <div class="card-dish__img-container">
+               <?php echo get_the_post_thumbnail($dish->ID, "", array(
+                  "class" => "card-dish__img",
+                  "alt" => ""
+               )); ?>
+            </div>
+            <h3 class="heading-secondary card-dish__name"><?php echo get_the_title($dish); ?></h3>
+            <p class="card-dish__price"><?php echo get_field("dish_price", $dish->ID); ?></p>
+            <p class="card-dish__desc"><?php echo get_field("dish_desc", $dish->ID); ?></p>
+         </div> <!-- end card dish -->
    <?php
+      }
+   } else {
+      echo "<p>Exceptionally, no dish will be cooked on that day this week</p>";
    }
+
    ?>
 </div>
 

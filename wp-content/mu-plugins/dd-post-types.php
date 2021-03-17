1ee0061fae871e7f87bbfc5886b286c3d143cbbb
anesofy <anne-sophie@digitalseeds.ch>
2021-03-17 15:11:02 +0000

    added a menu position

--- wp-content/mu-plugins/dd-post-types.php
+++ wp-content/mu-plugins/dd-post-types.php
@@ -1,43 +1,45 @@
-<?php 
-function digital_delicious_post_types() {
+<?php
+function digital_delicious_post_types()
+{
    /* Dish post type */
    $labels = array(
-      "name"   => _x( "Dishes", "digital-delicious" ),
-      "menu_name" => _x( "Dishes", "digital-delicious" ),
-      "singular_name" => _x( "Dish", "digital-delicious" ),
-      "add_new_item" => __( "Add New Dish", "digital-delicious" ),
-      "edit_item" => __( "Edit Dish", "digital-delicious" ),
-      "all_items" => __( "All Dishes", "digital-delicious" ),
+      "name"   => _x("Dishes", "digital-delicious"),
+      "menu_name" => _x("Dishes", "digital-delicious"),
+      "singular_name" => _x("Dish", "digital-delicious"),
+      "add_new_item" => __("Add New Dish", "digital-delicious"),
+      "edit_item" => __("Edit Dish", "digital-delicious"),
+      "all_items" => __("All Dishes", "digital-delicious"),
    );
    $args = array(
       "labels" => $labels,
       "public" => false,
-      "supports" => array( "title", "thumbnail" ),
+      "supports" => array("title", "thumbnail"),
       "show_ui" => true,
       "menu_icon" => "dashicons-food",
+      "menu_position" => 0,
    );
 
    register_post_type("dish", $args);
 
-    /* Service days post type */
-    $labels = array(
-        "name"   => _x( "Service Days", "digital-delicious" ),
-        "menu_name" => _x( "Service Days", "digital-delicious" ),
-        "singular_name" => _x( "Service Day", "digital-delicious" ),
-        "add_new_item" => __( "Add New Service Day", "digital-delicious" ),
-        "edit_item" => __( "Edit Service Day", "digital-delicious" ),
-        "all_items" => __( "All Service Days", "digital-delicious" ),
-     );
-     $args = array(
-        "labels" => $labels,
-        "public" => false,
-        "supports" => array( "title" ),
-        "show_ui" => true,
-        "menu_icon" => "dashicons-calendar",
-     );
+   /* Service days post type */
+   $labels = array(
+      "name"   => _x("Service Days", "digital-delicious"),
+      "menu_name" => _x("Service Days", "digital-delicious"),
+      "singular_name" => _x("Service Day", "digital-delicious"),
+      "add_new_item" => __("Add New Service Day", "digital-delicious"),
+      "edit_item" => __("Edit Service Day", "digital-delicious"),
+      "all_items" => __("All Service Days", "digital-delicious"),
+   );
+   $args = array(
+      "labels" => $labels,
+      "public" => false,
+      "supports" => array("title"),
+      "show_ui" => true,
+      "menu_icon" => "dashicons-calendar",
+      "menu_position" => 0,
+   );
 
-     register_post_type("service-days", $args);
-
+   register_post_type("service-days", $args);
 }
 
 add_action("init", "digital_delicious_post_types");

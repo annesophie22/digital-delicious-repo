4a3fb7fa46b22fc510b8e1154bd07d3f1a8991d9
anesofy <anne-sophie@digitalseeds.ch>
2021-03-17 10:34:50 +0000

    custom pages and posts layouts

--- wp-content/themes/digital-delicious/template-parts/content.php
+++ wp-content/themes/digital-delicious/template-parts/content.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template part for displaying posts
  *
@@ -10,16 +11,17 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php digital_delicious_post_thumbnail(); ?>
 	<header class="entry-header">
 		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
+		if (is_singular()) :
+			the_title('<h1 class="entry-title">', '</h1>');
 		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
 		endif;
 
-		if ( 'post' === get_post_type() ) :
-			?>
+		if ('post' === get_post_type()) :
+		?>
 			<div class="entry-meta">
 				<?php
 				digital_delicious_posted_on();
@@ -29,7 +31,7 @@
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<?php digital_delicious_post_thumbnail(); ?>
+	<!-- post thumbnail -->
 
 	<div class="entry-content">
 		<?php
@@ -37,20 +39,20 @@
 			sprintf(
 				wp_kses(
 					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'digital-delicious' ),
+					__('Continue reading<span class="screen-reader-text"> "%s"</span>', 'digital-delicious'),
 					array(
 						'span' => array(
 							'class' => array(),
 						),
 					)
 				),
-				wp_kses_post( get_the_title() )
+				wp_kses_post(get_the_title())
 			)
 		);
 
 		wp_link_pages(
 			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'digital-delicious' ),
+				'before' => '<div class="page-links">' . esc_html__('Pages:', 'digital-delicious'),
 				'after'  => '</div>',
 			)
 		);
